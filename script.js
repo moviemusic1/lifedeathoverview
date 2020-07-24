6682f8d50e9bf00a776e34ff2f28311ae894d1ed
Len <58519597+moviemusic1@users.noreply.github.com>
2020-07-24 10:39:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var bps = 4.25, dps = 1.75;
 var h1 = document.querySelector('#h1'), love_p = document.querySelector('#love'), event_p = document.querySelector('#event'), births = document.querySelector('#births'), deaths = document.querySelector('#deaths');
-var alife = 0;
+var alive = 0;
 var births_num = 0;
 var deaths_num = 0;
 
@@ -8,16 +8,16 @@ var birth_interval = setInterval(function() {
 	event_p.innerHTML = "Birth!";
 	event_p.classList.remove('death');
 	event_p.classList.add('birth');
-	alife += 1;
+	alive += 1;
 	births_num += 1;
 	update();
-	if(alife >= 100 && alife < 1000) {
+	if(alive >= 100 && alive < 1000) {
 		h1.style.fontSize = "9em";
 	}
-	if(alife >= 1000 && alife < 10000) {
+	if(alive >= 1000 && alive < 10000) {
 		h1.style.fontSize = "7em";
 	}
-	if(alife >= 10000) {
+	if(alive >= 10000) {
 		h1.style.fontSize = "5em";
 	}
 }, 1/bps * 1000);
@@ -26,13 +26,13 @@ var death_interval = setInterval(function() {
 	event_p.innerHTML = "Death.";
 	event_p.classList.remove('birth');
 	event_p.classList.add('death');
-	alife -= 1;
+	alive -= 1;
 	deaths_num += 1;
 	update();
 }, 1/dps * 1000);
 
 function update() {
-	h1.innerHTML = alife;
+	h1.innerHTML = alive;
 	births.innerHTML = births_num;
 	deaths.innerHTML = deaths_num;
 }
